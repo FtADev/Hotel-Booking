ae2966369b3c2c6563b6f470e5490374783d1dc2
fatemeh <akhlaghi.fatemeh@gmail.com>
2024-11-14 17:17:01 +0330

    Add DarkMode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
 import NavBar from "@/components/layout/NavBar";
 
 const geistSans = localFont({
@@ -41,10 +36,17 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <main className="flex flex-col min-h-screen bg-secondary">
-            <NavBar />
-            <section className="flex-grow">{children}</section>
-          </main>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main className="flex flex-col min-h-screen bg-secondary">
+              <NavBar />
+              <section className="flex-grow">{children}</section>
+            </main>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -7,6 +7,8 @@ import Image from "next/image";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
 import SearchInput from "../SearchInput";
+import { ModeToggle } from "../mode-toggle";
+import { NavMenu } from "./NavMenu";
 
 const NavBar = () => {
   const router = useRouter();
@@ -24,7 +26,10 @@ const NavBar = () => {
           </div>
           <SearchInput />
           <div className="flex gap-3 items-center">
-            <div>theme</div>
+            <div>
+              <ModeToggle />
+              <NavMenu />
+            </div>
             <UserButton afterSignOutUrl="/" />
             {!userId && (
               <>
