f53a37fd17cc25ecda659537100e0ff8cefbf746
fatemeh <akhlaghi.fatemeh@gmail.com>
2024-12-16 12:57:36 +0330

    Change Project Version

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { useAuth, UserButton } from "@clerk/clerk-react";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import Container from "../Container";
 import Image from "next/image";
 import { Button } from "../ui/button";
